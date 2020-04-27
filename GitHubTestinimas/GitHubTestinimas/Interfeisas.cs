e9a6bab435f3bcfec507baea02b8d03fbe1848c9
mantas95 <breivemantas@gmail.com>
2020-04-27 21:18:09 +0300

    r

---
+++ GitHubTestinimas/GitHubTestinimas/Interfeisas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTestinimas
+{
+    interface IHerojus
+    {
+        string Vardas { get; }
+        float HP { get; }
+        float Atack { get; }
+        float Defence { get; }
+
+        void Attack_target();
+        bool IsTargetDead(ITarget target);
+    }
+    interface ITarget
+    {
+        string Pavadinimas { get; set; }
+        float Hp { get; set; }
+        float Attack { get; set; }
+        float Defence { get; set; }
+
+
+    }
+
+}
